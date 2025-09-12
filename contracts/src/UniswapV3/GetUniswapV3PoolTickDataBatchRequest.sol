ad5b3877a87d4f307fbfd50572af07c448745ea1
Khanh Nguyen <clarkkent.k41@gmail.com>
2025-09-12 16:23:31 +0700

    fix lib not found

--- contracts/src/UniswapV3/GetUniswapV3PoolTickDataBatchRequest.sol
+++ contracts/src/UniswapV3/GetUniswapV3PoolTickDataBatchRequest.sol
@@ -1,7 +1,7 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../../lib/BytesLib.sol";
+import "../utils/BytesLib.sol";
 
 /**
  * @dev This contract is not meant to be deployed. Instead, use a static call with the
